870f41d84d493a03911b9003c2a34a36e929d961
Tom August <tomaug@ceh.ac.uk>
2018-01-23 15:35:02 +0000

    version 0.6.3

--- R/GetPackage.R
+++ R/GetPackage.R
@@ -1,34 +1,49 @@
 #' Helper to install (if needed) and load a package
-#' 
+#'
 #' Given a package name, either as a string or object,
 #' load the package if it exists, else install it from
 #' CRAN and then load
-#' 
+#'
 #' @param package A character vector of packages to load
 #' @return NULL
-#' @importFrom utils install.packages
-#' @examples GetPackage('gam')
+#' @importFrom utils install.packages installed.packages
 #' @export
+#' @examples
+#' \dontrun{
+#' 
+#' GetPackage('gam')
+#' 
+#' }
 
-GetPackage <- function (package) {
-  
+GetPackage <- function(package) {
+
   # Check package is a character
-  if(!inherits(x = package, what = 'character')) stop('package must be a character')
-  
-  for(i in package){
-    
-    # try loading and install and load if that doesn't work
-    if (!require(i,
-                 character.only = TRUE)) {
+  if (!inherits(x = package, what = "character"))
+    stop("package must be a character")
+
+  for (i in package) {
+
+    # if it isn't installed, install it
+    installed_packages <- rownames(installed.packages())
+
+    if (!i %in% installed_packages){
       
-      # otherwise use install.packages
-      install.packages(i,
-                       repos = "http://cran.rstudio.com")
-  
-      # now load the package
-      library(i,
-              character.only = TRUE)
+      cat(paste0('A module requires the package "',
+                i,
+                '". Would you like to install it?'))
+      
+      if(interactive()) {
+        installChoice <- utils::menu(c("yes", "no"))
+        if(installChoice == 1){
+          install.packages(i, repos = "http://cran.rstudio.com")
+        } else {
+          stop('Not installing packages and cannot continue.' )
+        }
+      }
     }
+    
+    # now load the package
+    library(i, character.only = TRUE)
+    
   }
 }
-
